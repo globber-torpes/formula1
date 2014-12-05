81a3326f0136ca8438d0f788071af5bf3e053817
polmdor <pablolmedorado@gmail.com>
2014-12-05 19:47:09 +0100

    Documentación Sphinx

--- src/Circuito.py
+++ src/Circuito.py
@@ -1,4 +1,6 @@
-class Circuito():
+class Circuito(object):
+    """Representa un circuito de Formula 1"""
+
     def __init__(self, nombre, ubicacion=None, longitud=None, num_vueltas=None):
         self.nombre = nombre
         self.ubicacion = ubicacion
@@ -6,11 +8,19 @@ class Circuito():
         self.num_vueltas = num_vueltas
 
     def __str__(self):
+        """
+        Metodo ToString
+
+        Muestra una representacion en texto de la instancia sobre la que se ejecuta.
+
+        :return: Cadena con los datos del circuito correspondiente
+        :rtype: String
+        """
         tostring = "Datos del circuito: \nNombre del circuito: " + self.nombre
-        if self.ubicacion is None:
+        if self.ubicacion is not None:
             tostring += "\nUbicacion: " + self.ubicacion
-        if self.longitud is None:
+        if self.longitud is not None:
             tostring += "\nLongitud: " + str(self.longitud) + "km"
-        if self.num_vueltas is None:
+        if self.num_vueltas is not None:
             tostring += "\nNumero de Vueltas: " + str(self.num_vueltas)
         return tostring + "\n"
--- tests/test_circuito.py
+++ tests/test_circuito.py
@@ -1,12 +1,19 @@
 __author__ = 'pablolmedorado'
 
 from unittest import TestCase
-
 from src.Circuito import Circuito
 
 
 class TestCircuito(TestCase):
     def test_str(self):
+        """
+        Prueba del metodo ToString
+
+        Dados unos datos, comprueba que se crea correctamente el objeto Circuito y que muestra bien los datos
+
+        :return: Resultado de la prueba
+        :rtype: Boolean
+        """
         circ = Circuito("CircuitoDePrueba", "UbDePrueba", 12345, 99)
         string = "Datos del circuito: \nNombre del circuito: " + "CircuitoDePrueba"
         string += "\nUbicacion: " + "UbDePrueba"
